aa74f64822a29d4b79ec4a111f175082a410d168
Timofey Stepanov <tstepanov@google.com>
2024-04-18 04:01:33 -0700

    `JaggedShapeEquivalentTo` matcher
    We may consider turning it into some generic `IsEquivalentTo` matcher later.
    
    Example:
    ```
    ERROR: No public description
    Value of: shape1
    Expected: is equivalent to JaggedShape(2, [1, 3])
      Actual: 0x301e7fe07aa0, pointing to JaggedShape(2, [1, 2]) which is not equivalent
    Stack trace:
    0x7f5deecda7cd: arolla::(anonymous namespace)::QTypeTest_JaggedShapeEquivalentTo_Test::TestBody() @ ??:??
    0x7f5d9afc0c92: testing::Test::Run() @ ??:??
    0x7f5d9afc1bb9: testing::TestInfo::Run() @ ??:??
    ... Google Test internal frames ...
    ```
    PiperOrigin-RevId: 625981957
    Change-Id: I4bc1e1de6fdc3b457b789d3c02ee0e9fa0d9b136

---
+++ arolla/jagged_shape/testing/matchers.h
@@ -0,0 +1,91 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+#ifndef AROLLA_JAGGED_SHAPE_TESTING_MATCHERS_H_
+#define AROLLA_JAGGED_SHAPE_TESTING_MATCHERS_H_
+
+#include <ostream>
+
+#include "gtest/gtest.h"
+#include "absl/log/check.h"
+#include "arolla/jagged_shape/jagged_shape.h"
+#include "arolla/util/repr.h"
+
+namespace arolla::testing {
+
+namespace matchers_impl {
+
+template <typename Edge>
+class JaggedShapeEquivalentToMatcher {
+ public:
+  using is_gtest_matcher = void;
+
+  explicit JaggedShapeEquivalentToMatcher(JaggedShapePtr<Edge> expected_shape)
+      : expected_shape_(std::move(expected_shape)) {
+    DCHECK(expected_shape_ != nullptr);
+  }
+
+  bool MatchAndExplain(const JaggedShapePtr<Edge>& shape,
+                       ::testing::MatchResultListener* listener) const {
+    if (shape == nullptr) {
+      *listener << "is null";
+      return false;
+    }
+    *listener << "pointing to ";
+    return MatchAndExplain(*shape, listener);
+  }
+
+  bool MatchAndExplain(const JaggedShape<Edge>& shape,
+                       ::testing::MatchResultListener* listener) const {
+    bool is_equivalent = shape.IsEquivalentTo(*expected_shape_);
+    *listener << Repr(shape)
+              << (is_equivalent ? " which is equivalent"
+                                : " which is not equivalent");
+    return is_equivalent;
+  }
+
+  void DescribeTo(::std::ostream* os) const {
+    *os << "is equivalent to " << Repr(*expected_shape_);
+  }
+
+  void DescribeNegationTo(::std::ostream* os) const {
+    *os << "is not equivalent to " << Repr(*expected_shape_);
+  }
+
+ private:
+  JaggedShapePtr<Edge> expected_shape_;
+};
+
+}  // namespace matchers_impl
+
+// Returns GMock matcher for JaggedShapePtr.
+//
+// Usage:
+//   EXPECT_THAT(my_shape, JaggedShapeEquivalentTo(value_matcher));
+//
+template <typename Edge>
+matchers_impl::JaggedShapeEquivalentToMatcher<Edge> JaggedShapeEquivalentTo(
+    const JaggedShapePtr<Edge>& expected_shape) {
+  return matchers_impl::JaggedShapeEquivalentToMatcher<Edge>(expected_shape);
+}
+template <typename Edge>
+matchers_impl::JaggedShapeEquivalentToMatcher<Edge> JaggedShapeEquivalentTo(
+    const JaggedShape<Edge>& expected_shape) {
+  return matchers_impl::JaggedShapeEquivalentToMatcher<Edge>(
+      JaggedShapePtr<Edge>::NewRef(&expected_shape));
+}
+
+}  // namespace arolla::testing
+
+#endif  // AROLLA_JAGGED_SHAPE_TESTING_MATCHERS_H_
---
+++ arolla/jagged_shape/testing/matchers_test.cc
@@ -0,0 +1,74 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+#include "arolla/jagged_shape/testing/matchers.h"
+
+#include <cstdint>
+#include <string>
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "arolla/dense_array/dense_array.h"
+#include "arolla/dense_array/edge.h"
+#include "arolla/jagged_shape/dense_array/jagged_shape.h"
+
+namespace arolla {
+namespace {
+
+using ::arolla::testing::JaggedShapeEquivalentTo;
+using ::testing::Eq;
+using ::testing::Not;
+using ::testing::StringMatchResultListener;
+
+template <typename MatcherType, typename Value>
+std::string Explain(const MatcherType& m, const Value& x) {
+  StringMatchResultListener listener;
+  ExplainMatchResult(m, x, &listener);
+  return listener.str();
+}
+
+TEST(QTypeTest, JaggedShapeEquivalentTo) {
+  ASSERT_OK_AND_ASSIGN(auto edge1, DenseArrayEdge::FromSplitPoints(
+                                       CreateDenseArray<int64_t>({0, 2})));
+  ASSERT_OK_AND_ASSIGN(auto edge2, DenseArrayEdge::FromSplitPoints(
+                                       CreateDenseArray<int64_t>({0, 1, 3})));
+  ASSERT_OK_AND_ASSIGN(auto shape1,
+                       JaggedDenseArrayShape::FromEdges({edge1, edge2}));
+  ASSERT_OK_AND_ASSIGN(auto edge3, DenseArrayEdge::FromSplitPoints(
+                                       CreateDenseArray<int64_t>({0, 1, 4})));
+  ASSERT_OK_AND_ASSIGN(auto shape2,
+                       JaggedDenseArrayShape::FromEdges({edge1, edge3}));
+
+  EXPECT_THAT(shape1, JaggedShapeEquivalentTo(shape1));
+  EXPECT_THAT(shape1, Not(JaggedShapeEquivalentTo(shape2)));
+
+  auto m = JaggedShapeEquivalentTo(shape1);
+  EXPECT_THAT(::testing::DescribeMatcher<JaggedDenseArrayShapePtr>(m),
+              Eq("is equivalent to JaggedShape(2, [1, 2])"));
+  EXPECT_THAT(::testing::DescribeMatcher<JaggedDenseArrayShapePtr>(
+                  m, /*negation=*/true),
+              Eq("is not equivalent to JaggedShape(2, [1, 2])"));
+  EXPECT_THAT(Explain(m, nullptr), Eq("is null"));
+  EXPECT_THAT(Explain(m, *shape1),
+              Eq("JaggedShape(2, [1, 2]) which is equivalent"));
+  EXPECT_THAT(Explain(m, shape1),
+              Eq("pointing to JaggedShape(2, [1, 2]) which is equivalent"));
+  EXPECT_THAT(Explain(m, *shape2),
+              Eq("JaggedShape(2, [1, 3]) which is not equivalent"));
+  EXPECT_THAT(Explain(m, shape2),
+              Eq("pointing to JaggedShape(2, [1, 3]) which is not equivalent"));
+}
+
+}  // namespace
+}  // namespace arolla
