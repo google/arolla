1752561acfa2dc80f9a2e728ec774bade255ddfd
Timofey Stepanov <tstepanov@google.com>
2024-05-22 00:05:29 -0700

    `AROLLA_REGISTER_QEXPR_OPERATOR` and `AROLLA_REGISTER_QEXPR_OPERATOR_FAMILY` macros
    They will work as a replacement to `operator_libraries` and `operator_family` BUILD rules in cases when there only one (or a few) QType signatures per operator and no metadata for Codegen is needed.
    
    PiperOrigin-RevId: 636056430
    Change-Id: If6d65d5676d3f16a29c008fa744c6bbda574d31b

---
+++ arolla/qexpr/optools.h
@@ -0,0 +1,84 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+#ifndef AROLLA_QEXPR_OPTOOLS_H_
+#define AROLLA_QEXPR_OPTOOLS_H_
+
+#include <memory>  // IWYU pragma: keep
+
+#include "arolla/qexpr/operator_factory.h"  // IWYU pragma: keep
+#include "arolla/util/init_arolla.h"  // IWYU pragma: keep
+#include "arolla/qexpr/operators.h"  // IWYU pragma: keep
+#include "arolla/util/status_macros_backport.h"  // IWYU pragma: keep
+
+// Creates and registers a QExpr operator from a function.
+//
+// The operator input and output QTypes are inferred from the function. One can
+// register several operators with the same name, but with different signatures.
+//
+// NOTE: this macro is a simpler alternative to operator_libraries BUILD rule.
+// Unlike it, the macro does not register metadata for Codegen, so the operator
+// will be available only in Dynamic Eval.
+//
+// The macro must only be used from a *.cc file in an alwayslink=1 cc_library.
+//
+// Example usages:
+//
+// AROLLA_REGISTER_QEXPR_OPERATOR("my_namespace.add", [](float a, float b) {
+//       return a + b;
+//     });
+//
+// double AddDoubles(double a, double b) { return a + b; }
+// AROLLA_REGISTER_QEXPR_OPERATOR("my_namespace.add", AddDoubles);
+//
+// template <typename T>
+// struct AddTs {
+//   T operator()(const T& a, const T& b) const { return a + b; }
+// };
+// AROLLA_REGISTER_QEXPR_OPERATOR("my_namespace.add", AddTs<int64_t>());
+//
+#define AROLLA_REGISTER_QEXPR_OPERATOR(op_name, op_fn)                     \
+  AROLLA_REGISTER_ANONYMOUS_INITIALIZER(                                   \
+      kRegisterQExprOperators, []() -> absl::Status {                      \
+        ASSIGN_OR_RETURN(                                                  \
+            auto op,                                                       \
+            arolla::OperatorFactory().WithName(op_name).BuildFromFunction( \
+                op_fn));                                                   \
+        return arolla::OperatorRegistry::GetInstance()->RegisterOperator(  \
+            std::move(op));                                                \
+      });
+
+// Registers a QExpr OperatorFamily.
+//
+// NOTE: this macro is a simpler alternative to operator_family BUILD rule.
+// Unlike it, the macro does not register metadata for Codegen, so the operator
+// will be available only in Dynamic Eval.
+//
+// The macro must only be used from a *.cc file in an alwayslink=1 cc_library.
+//
+// Example usage:
+//
+// class AddFamily : public arolla::OperatorFamily {
+//    // ...
+// };
+// AROLLA_REGISTER_QEXPR_OPERATOR_FAMILY("my_namespace.add", AddFamily);
+//
+#define AROLLA_REGISTER_QEXPR_OPERATOR_FAMILY(op_name, op_family)             \
+  AROLLA_REGISTER_ANONYMOUS_INITIALIZER(                                      \
+      kRegisterQExprOperators, []() -> absl::Status {                         \
+        return arolla::OperatorRegistry::GetInstance()                        \
+            ->RegisterOperatorFamily(op_name, std::make_unique<op_family>()); \
+      });
+
+#endif  // AROLLA_QEXPR_OPTOOLS_H_
---
+++ arolla/qexpr/optools_test.cc
@@ -0,0 +1,99 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+#include "arolla/qexpr/optools.h"
+
+#include <cstdint>
+#include <string>
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "absl/status/status.h"
+#include "absl/status/statusor.h"
+#include "absl/strings/str_cat.h"
+#include "absl/types/span.h"
+#include "arolla/qexpr/operator_factory.h"
+#include "arolla/qexpr/operators.h"
+#include "arolla/qtype/qtype.h"
+#include "arolla/qtype/qtype_traits.h"
+#include "arolla/util/init_arolla.h"
+#include "arolla/util/testing/status_matchers_backport.h"
+
+namespace test_namespace {
+namespace {
+using ::arolla::testing::IsOkAndHolds;
+using ::testing::Eq;
+
+AROLLA_REGISTER_QEXPR_OPERATOR("optools_test.to_string_from_lambda",
+                               [](int32_t x) { return absl::StrCat(x); });
+
+std::string ToString(int32_t x) { return absl::StrCat(x); }
+AROLLA_REGISTER_QEXPR_OPERATOR("optools_test.to_string_from_function",
+                               ToString);
+
+template <typename T>
+struct ToStringOp {
+  std::string operator()(T x) const { return absl::StrCat(x); }
+};
+AROLLA_REGISTER_QEXPR_OPERATOR("optools_test.to_string_from_functor",
+                               ToStringOp<int32_t>());
+
+class ToStringOperatorFamily : public arolla::OperatorFamily {
+ public:
+  absl::StatusOr<arolla::OperatorPtr> DoGetOperator(
+      absl::Span<const arolla::QTypePtr> input_types,
+      arolla::QTypePtr output_type) const final {
+    if (input_types.size() != 1 ||
+        input_types[0] != arolla::GetQType<int32_t>()) {
+      return absl::InvalidArgumentError(
+          "the only supported input type is int32");
+    }
+    return arolla::OperatorFactory()
+        .WithName("optools_test.to_string_family")
+        .BuildFromFunction(ToString);
+  }
+};
+AROLLA_REGISTER_QEXPR_OPERATOR_FAMILY("optools_test.to_string_from_family",
+                                      ToStringOperatorFamily);
+
+struct OptoolsTest : public ::testing::Test {
+  void SetUp() override { ASSERT_OK(arolla::InitArolla()); }
+};
+
+TEST_F(OptoolsTest, FromFunction) {
+  EXPECT_THAT(arolla::InvokeOperator<std::string>(
+                  "optools_test.to_string_from_function", 57),
+              IsOkAndHolds(Eq("57")));
+}
+
+TEST_F(OptoolsTest, FromLambda) {
+  EXPECT_THAT(arolla::InvokeOperator<std::string>(
+                  "optools_test.to_string_from_lambda", 57),
+              IsOkAndHolds(Eq("57")));
+}
+
+TEST_F(OptoolsTest, FromFunctor) {
+  EXPECT_THAT(arolla::InvokeOperator<std::string>(
+                  "optools_test.to_string_from_functor", 57),
+              IsOkAndHolds(Eq("57")));
+}
+
+TEST_F(OptoolsTest, FromFamily) {
+  EXPECT_THAT(arolla::InvokeOperator<std::string>(
+                  "optools_test.to_string_from_family", 57),
+              IsOkAndHolds(Eq("57")));
+}
+
+}  // namespace
+}  // namespace test_namespace
