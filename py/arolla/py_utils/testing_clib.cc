4257977a4a275901c3c5011d1dd8a41b0bd10625
Arolla Team <noreply@google.com>
2024-05-21 22:38:56 -0700

    No public description
    PiperOrigin-RevId: 636037820
    Change-Id: If60033183284c8422efef0584b1b9cee96070a83

--- py/arolla/py_utils/testing_clib.cc
+++ py/arolla/py_utils/testing_clib.cc
@@ -19,6 +19,7 @@
 #include <utility>
 
 #include "absl/status/status.h"
+#include "absl/strings/cord.h"
 #include "absl/strings/string_view.h"
 #include "py/arolla/py_utils/py_object_as_status_payload.h"
 #include "py/arolla/py_utils/py_utils.h"
