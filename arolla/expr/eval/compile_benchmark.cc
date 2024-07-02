9c5c04f35875fe8ec829eda638d6965fcb6b4bdb
Alexander Pronchenkov <axv@google.com>
2024-07-02 05:51:03 -0700

    Add a couple of benchmarks for expr/eval
    Motivation: The initial intention was to evaluate the e2e effect of recent changes in the QExpr subsystem. However, having a benchmark can be useful for assessing future changes.
    
    Benchmark results:
    ```
    name                                      old cpu/op   new cpu/op   delta
    BM_Add_F32_F32_NTimes_WithStacktrace      177ms ± 7%   174ms ± 5%   -1.81%  (p=0.000 n=89+77)
    BM_Add_F32_F32_NTimes_WithoutStacktrace   163ms ± 5%   161ms ± 4%   -1.57%  (p=0.000 n=84+80)
    BM_Add_F64_F32_NTimes_WithStacktrace      229ms ± 7%   208ms ± 9%   -9.45%  (p=0.000 n=85+78)
    BM_Add_F64_F32_NTimes_WithoutStacktrace   212ms ± 5%   190ms ± 7%  -10.36%  (p=0.000 n=85+81)
    ```
    
    PiperOrigin-RevId: 648688474
    Change-Id: I6d322ec8bf5f8f45bbda4b048aaf1ede51b8589c

---
+++ arolla/expr/eval/compile_benchmark.cc
@@ -0,0 +1,100 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+#include "benchmark/benchmark.h"
+#include "absl/log/check.h"
+#include "arolla/expr/eval/model_executor.h"
+#include "arolla/expr/expr.h"
+#include "arolla/io/wildcard_input_loader.h"
+#include "arolla/qtype/base_types.h"
+#include "arolla/util/init_arolla.h"
+
+namespace {
+
+using ::arolla::InitArolla;
+using ::arolla::WildcardInputLoader;
+using ::arolla::expr::CallOp;
+using ::arolla::expr::CompileModelExecutor;
+using ::arolla::expr::Leaf;
+using ::arolla::expr::Literal;
+using ::arolla::expr::ModelEvaluationOptions;
+using ::arolla::expr::ModelExecutorOptions;
+
+constexpr int N = 10'000;
+
+void Add_F32_F32_NTimes(int N, bool enable_expr_stack_trace,
+                        benchmark::State& state) {
+  CHECK_OK(InitArolla());
+  auto leaf = Leaf("x");
+  auto expr = Literal<float>(0.0f);
+  for (int i = 0; i < N; ++i) {
+    expr = *CallOp("math.add", {expr, leaf});
+  }
+  auto accessor = [](float x, absl::string_view) { return x; };
+  for (auto _ : state) {
+    auto input_loader = *WildcardInputLoader<float>::Build(accessor);
+    auto model_executor = *CompileModelExecutor<float>(
+        expr, *input_loader,
+        ModelExecutorOptions{.eval_options = {.enable_expr_stack_trace =
+                                                  enable_expr_stack_trace}});
+    auto y =
+        *model_executor.ExecuteOnStack<128>(ModelEvaluationOptions{}, 1.0f);
+    benchmark::DoNotOptimize(y);
+  }
+}
+
+void BM_Add_F32_F32_NTimes_WithoutStacktrace(benchmark::State& state) {
+  Add_F32_F32_NTimes(N, false, state);
+}
+
+void BM_Add_F32_F32_NTimes_WithStacktrace(benchmark::State& state) {
+  Add_F32_F32_NTimes(N, true, state);
+}
+
+BENCHMARK(BM_Add_F32_F32_NTimes_WithStacktrace);
+BENCHMARK(BM_Add_F32_F32_NTimes_WithoutStacktrace);
+
+void Add_F64_F32_NTimes(int N, bool enable_expr_stack_trace,
+                        benchmark::State& state) {
+  CHECK_OK(InitArolla());
+  auto leaf = Leaf("x");
+  auto expr = Literal<double>(0.0f);
+  for (int i = 0; i < N; ++i) {
+    expr = *CallOp("math.add", {expr, leaf});
+  }
+  auto accessor = [](float x, absl::string_view) { return x; };
+  for (auto _ : state) {
+    auto input_loader = *WildcardInputLoader<float>::Build(accessor);
+    auto model_executor = *CompileModelExecutor<double>(
+        expr, *input_loader,
+        ModelExecutorOptions{.eval_options = {.enable_expr_stack_trace =
+                                                  enable_expr_stack_trace}});
+    auto y =
+        *model_executor.ExecuteOnStack<128>(ModelEvaluationOptions{}, 1.0f);
+    benchmark::DoNotOptimize(y);
+  }
+}
+
+void BM_Add_F64_F32_NTimes_WithoutStacktrace(benchmark::State& state) {
+  Add_F64_F32_NTimes(N, false, state);
+}
+
+void BM_Add_F64_F32_NTimes_WithStacktrace(benchmark::State& state) {
+  Add_F64_F32_NTimes(N, true, state);
+}
+
+BENCHMARK(BM_Add_F64_F32_NTimes_WithStacktrace);
+BENCHMARK(BM_Add_F64_F32_NTimes_WithoutStacktrace);
+
+}  // namespace
