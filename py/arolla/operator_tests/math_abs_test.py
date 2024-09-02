b0d517b9ae4c1c05328c9167276af1cc0fe1b8ab
Arolla Team <noreply@google.com>
2024-09-02 02:31:19 -0700

    Add kde.abs operator.
    PiperOrigin-RevId: 670138677
    Change-Id: I3aceb918254db2631223e12f5c9601d119fb555b

--- py/arolla/operator_tests/math_abs_test.py
+++ py/arolla/operator_tests/math_abs_test.py
@@ -17,6 +17,7 @@
 from absl.testing import absltest
 from absl.testing import parameterized
 from arolla import arolla
+from arolla.operator_tests import backend_test_base
 from arolla.operator_tests import pointwise_test_utils
 
 NAN = float("nan")
@@ -56,16 +57,17 @@ QTYPE_SIGNATURES = tuple(
 )
 
 
-class MathAbsTest(parameterized.TestCase):
+class MathAbsTest(parameterized.TestCase, backend_test_base.SelfEvalMixin):
 
   def testQTypeSignatures(self):
+    self.require_self_eval_is_called = False
     arolla.testing.assert_qtype_signatures(M.math.abs, QTYPE_SIGNATURES)
 
   @parameterized.parameters(
       pointwise_test_utils.gen_cases(TEST_DATA, *QTYPE_SIGNATURES)
   )
   def testValue(self, arg, expected_value):
-    actual_value = arolla.eval(M.math.abs(arg))
+    actual_value = self.eval(M.math.abs(arg))
     arolla.testing.assert_qvalue_allequal(actual_value, expected_value)
 
 
