5874d7835fc78841e2f6d19a8a6b5073cc71eae7
Arolla Team <noreply@google.com>
2024-09-13 02:34:46 -0700

    No public description
    PiperOrigin-RevId: 674213959
    Change-Id: I89501a0646e8d95f6316aa7aac7b51e9a89bdffe

--- arolla/expr/eval/model_executor.cc
+++ arolla/expr/eval/model_executor.cc
@@ -300,8 +300,8 @@ std::unique_ptr<CompiledExpr> CastOutputsIfNeeded(
       }
     }
   }
-  return std::make_unique<CastingCompiledExpr>(
-      std::move(expr), desired_output_type, side_output_types, options);
+  return std::make_unique<CastingCompiledExpr>(expr, desired_output_type,
+                                               side_output_types, options);
 }
 
 absl::Status VerifyAllNamedOutputsAreListened(
