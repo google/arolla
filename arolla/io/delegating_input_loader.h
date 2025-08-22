8b574eb3838a117160eb4ed7bf3d82c39adfafc2
Vitaly Goldshteyn <goldvitaly@google.com>
2025-08-22 06:51:22 -0700

    `CreateDynamicDelegatingInputLoader` that simplifies combining InputLoader's. In particular in order to access proto `map<string, OtherProro>`.
    PiperOrigin-RevId: 798193238
    Change-Id: I8eeb1a68e1068695f07931ffa3b9eb27151234a2

--- arolla/io/delegating_input_loader.h
+++ arolla/io/delegating_input_loader.h
@@ -17,19 +17,23 @@
 
 #include <functional>
 #include <memory>
+#include <optional>
 #include <string>
 #include <type_traits>
 #include <utility>
 #include <vector>
 
 #include "absl/base/nullability.h"
+#include "absl/base/thread_annotations.h"
 #include "absl/container/flat_hash_map.h"
+#include "absl/functional/any_invocable.h"
 #include "absl/status/status.h"
 #include "absl/status/statusor.h"
 #include "absl/strings/str_cat.h"
 #include "absl/strings/str_format.h"
 #include "absl/strings/string_view.h"
 #include "absl/strings/strip.h"
+#include "absl/synchronization/mutex.h"
 #include "arolla/io/input_loader.h"
 #include "arolla/memory/frame.h"
 #include "arolla/memory/raw_buffer_factory.h"
@@ -57,7 +61,7 @@ class DelegatingInputLoader final : public InputLoader<Input> {
           delegate_loader,
       std::function<DelegateInputFullType(const Input&)> accessor,
       std::string name_prefix) {
-    // Not using make_shared to avoid binary size blowup.
+    // Not using make_unique to avoid binary size blowup.
     return std::unique_ptr<InputLoader<Input>>(
         static_cast<InputLoader<Input>*>(new DelegatingInputLoader(
             std::move(delegate_loader), accessor, std::move(name_prefix))));
@@ -113,6 +117,119 @@ class DelegatingInputLoader final : public InputLoader<Input> {
   std::unique_ptr<InputLoader<DelegateInput>> delegate_loader_;
   std::function<DelegateInputFullType(const Input&)> accessor_;
   std::string name_prefix_;
+};
+
+template <class Input>
+using DelegateLoaderFactoryRawFn =
+    absl::StatusOr<std::unique_ptr<InputLoader<Input>>>(absl::string_view);
+
+template <class Input>
+using DelegateLoaderFactory = std::function<DelegateLoaderFactoryRawFn<Input>>;
+
+// InputLoader delegating to many loaders created on demand via factory
+// function.
+template <class Input>
+class DynamicDelegatingInputLoader final : public InputLoader<Input> {
+ public:
+  // Prefer CreateDynamicDelegatingInputLoader for type deduction.
+  static absl::StatusOr<std::unique_ptr<InputLoader<Input>>> Build(
+      DelegateLoaderFactory<Input> delegate_loader_factory,
+      absl::AnyInvocable<std::optional<std::string>(absl::string_view) const>
+          name2key,
+      std::vector<std::string> suggest_available_names) {
+    // Not using make_unique to avoid binary size blowup.
+    return std::unique_ptr<InputLoader<Input>>(
+        static_cast<InputLoader<Input>*>(new DynamicDelegatingInputLoader(
+            std::move(delegate_loader_factory), std::move(name2key),
+            std::move(suggest_available_names))));
+  }
+
+  const QType* absl_nullable GetQTypeOf(absl::string_view name) const final {
+    std::optional<std::string> key = name2key_(name);
+    if (!key.has_value()) {
+      return nullptr;
+    }
+    auto delegate_loader = GetDelegateLoader(*key);
+    if (!delegate_loader.ok()) {
+      return nullptr;
+    }
+    return (*delegate_loader)->GetQTypeOf(name);
+  }
+
+  std::vector<std::string> SuggestAvailableNames() const final {
+    return suggest_available_names_;
+  }
+
+ private:
+  DynamicDelegatingInputLoader(
+      DelegateLoaderFactory<Input> delegate_loader_factory,
+      absl::AnyInvocable<std::optional<std::string>(absl::string_view) const>
+          name2key,
+      std::vector<std::string> suggest_available_names)
+      : delegate_loader_factory_(std::move(delegate_loader_factory)),
+        name2key_(std::move(name2key)),
+        suggest_available_names_(std::move(suggest_available_names)) {}
+
+  absl::StatusOr<BoundInputLoader<Input>> BindImpl(
+      const absl::flat_hash_map<std::string, TypedSlot>& output_slots)
+      const final {
+    absl::flat_hash_map<std::string,
+                        absl::flat_hash_map<std::string, TypedSlot>>
+        delegate_output_slots;
+    for (const auto& [name, slot] : output_slots) {
+      std::optional<std::string> key = name2key_(name);
+      if (!key.has_value()) {
+        // Should be already validated in InputLoader::Bind().
+        return absl::InternalError(
+            absl::StrFormat("unexpected input name %s", name));
+      }
+      delegate_output_slots[*key].emplace(name, slot);
+    }
+    std::vector<BoundInputLoader<Input>> bound_loaders;
+    for (const auto& [key, delegate_output_slots] : delegate_output_slots) {
+      ASSIGN_OR_RETURN(auto* delegate_loader, GetDelegateLoader(key));
+      ASSIGN_OR_RETURN(BoundInputLoader<Input> bound_delegate_loader,
+                       delegate_loader->Bind(delegate_output_slots));
+      bound_loaders.push_back(std::move(bound_delegate_loader));
+    }
+    if (bound_loaders.empty()) {
+      return absl::InternalError(
+          "no slots were bound, must be processed in Bind");
+    }
+    // Avoid indirection in case only one loader is bound.
+    if (bound_loaders.size() == 1) {
+      return std::move(bound_loaders[0]);
+    }
+    return BoundInputLoader<Input>(
+        [bound_loaders(std::move(bound_loaders))](
+            const Input& input, FramePtr frame,
+            RawBufferFactory* factory) -> absl::Status {
+          return ChainInputLoader<Input>::InvokeBoundLoaders(
+              bound_loaders, input, frame, factory);
+        });
+  }
+
+  absl::StatusOr<const InputLoader<Input>*> GetDelegateLoader(
+      absl::string_view key) const {
+    absl::MutexLock lock(&mutex_);
+    auto it = delegate_loaders_.find(key);
+    if (it != delegate_loaders_.end()) {
+      return it->second.get();
+    }
+    ASSIGN_OR_RETURN(auto loader, delegate_loader_factory_(key));
+    const auto* result = loader.get();
+    delegate_loaders_[key] = std::move(loader);
+    return result;
+  }
+
+  DelegateLoaderFactory<Input> delegate_loader_factory_;
+  absl::AnyInvocable<std::optional<std::string>(absl::string_view) const>
+      name2key_;
+  std::vector<std::string> suggest_available_names_;
+
+  mutable absl::Mutex mutex_;
+  mutable absl::flat_hash_map<std::string, std::unique_ptr<InputLoader<Input>>>
+      delegate_loaders_ ABSL_GUARDED_BY(mutex_);
 };
 
 }  // namespace delegating_input_loader_impl
@@ -194,6 +311,59 @@ CreateDelegatingInputLoaderWithCopyAllowed(
   return delegating_input_loader_impl::DelegatingInputLoader<
       Input, DelegateInput>::Build(std::move(delegate_loader), accessor,
                                    std::move(name_prefix));
+}
+
+// Creates InputLoader that delegates to dynamically created loaders.
+//
+// Delegate loaders are created on demand via factory function, which is
+// expected to return InputLoader<Input>.
+// Name2Key function is used to convert input name to a key used to create
+// a loader.
+//
+// Usage example:
+//
+// struct Input {
+//   int a;
+//   int b;
+// };
+//
+// ASSIGN_OR_RETURN(
+//     auto loader,
+//     arolla::CreateDynamicDelegatingInputLoader<Input>(
+//         [](absl::string_view key)
+//             -> absl::StatusOr<std::unique_ptr<InputLoader<Input>>> {
+//           if (key == "a") {
+//             return CreateAccessorsInputLoader<Input>(
+//                 "get(a)", [](const Input& s) { return s.a; });
+//           }
+//           if (key == "b") {
+//             return CreateAccessorsInputLoader<Input>(
+//                 "get(b)", [](const Input& s) { return s.b; });
+//           }
+//           return absl::NotFoundError(absl::StrCat(key, " not found"));
+//         },
+//         [](absl::string_view name) -> std::optional<std::string> {
+//           if (absl::StartsWith(name, "get(a)")) {
+//             return "a";
+//           }
+//           if (absl::StartsWith(name, "get(b)")) {
+//             return "b";
+//           }
+//           return std::nullopt;
+//         },
+//         /*suggest_available_names=*/{"get(a)", "get(b)"}));
+//
+template <class Input>
+absl::StatusOr<std::unique_ptr<InputLoader<Input>>>
+CreateDynamicDelegatingInputLoader(
+    delegating_input_loader_impl::DelegateLoaderFactory<Input>
+        delegate_loader_factory,
+    absl::AnyInvocable<std::optional<std::string>(absl::string_view) const>
+        name2key,
+    std::vector<std::string> suggest_available_names) {
+  return delegating_input_loader_impl::DynamicDelegatingInputLoader<
+      Input>::Build(std::move(delegate_loader_factory), std::move(name2key),
+                    std::move(suggest_available_names));
 }
 
 }  // namespace arolla
--- arolla/io/delegating_input_loader_test.cc
+++ arolla/io/delegating_input_loader_test.cc
@@ -16,14 +16,20 @@
 
 #include <cstdint>
 #include <memory>
+#include <optional>
 #include <string>
 #include <utility>
 #include <vector>
 
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
+#include "absl/functional/any_invocable.h"
 #include "absl/log/check.h"
+#include "absl/status/status.h"
 #include "absl/status/statusor.h"
+#include "absl/strings/match.h"
+#include "absl/strings/str_cat.h"
+#include "absl/strings/string_view.h"
 #include "absl/types/span.h"
 #include "arolla/io/accessors_input_loader.h"
 #include "arolla/io/input_loader.h"
@@ -115,6 +121,99 @@ TEST(InputLoaderTest, DelegatingInputLoader) {
     ASSERT_OK(bound_input_loader({&ts, -1}, alloc.frame()));
     EXPECT_EQ(alloc.frame().Get(a_slot), 5);
     EXPECT_EQ(alloc.frame().Get(b_slot), 3.5);
+  }
+}
+
+TEST(InputLoaderTest, DynamicDelegatingInputLoader) {
+  auto i32 = GetQType<int32_t>();
+  auto f64 = GetQType<double>();
+  ASSERT_OK_AND_ASSIGN(
+      std::unique_ptr<InputLoader<OuterTestStruct>> input_loader,
+      CreateDynamicDelegatingInputLoader<OuterTestStruct>(
+          [](absl::string_view key)
+              -> absl::StatusOr<std::unique_ptr<InputLoader<OuterTestStruct>>> {
+            if (key == "a") {
+              return CreateAccessorsInputLoader<OuterTestStruct>(
+                  "get(a).a", [](const OuterTestStruct& s) { return s.a->a; },
+                  "get(a).b", [](const OuterTestStruct& s) { return s.a->b; });
+            }
+            if (key == "b") {
+              return CreateAccessorsInputLoader<OuterTestStruct>(
+                  "get(b)", [](const OuterTestStruct& s) { return s.b; });
+            }
+            return absl::NotFoundError(
+                absl::StrCat("key `", key, "` not found"));
+          },
+          [](absl::string_view name) -> std::optional<std::string> {
+            if (absl::StartsWith(name, "get(a)")) {
+              return "a";
+            }
+            if (absl::StartsWith(name, "get(b)")) {
+              return "b";
+            }
+            return std::nullopt;
+          },
+          {"get(a).a", "get(a).b", "get(b)"}));
+  EXPECT_THAT(*input_loader,
+              InputLoaderSupports(
+                  {{"get(a).a", i32}, {"get(a).b", f64}, {"get(b)", i32}}));
+  EXPECT_EQ(input_loader->GetQTypeOf("get(c)"), nullptr);
+  EXPECT_EQ(input_loader->GetQTypeOf("get(a)"), nullptr);
+  EXPECT_EQ(input_loader->GetQTypeOf("get(a).x"), nullptr);
+  EXPECT_EQ(input_loader->GetQTypeOf("get(b).a"), nullptr);
+  EXPECT_THAT(input_loader->SuggestAvailableNames(),
+              ::testing::ElementsAre("get(a).a", "get(a).b", "get(b)"));
+
+  FrameLayout::Builder layout_builder;
+  auto a_slot = layout_builder.AddSlot<int>();
+  auto b_slot = layout_builder.AddSlot<double>();
+  auto top_b_slot = layout_builder.AddSlot<int>();
+  FrameLayout memory_layout = std::move(layout_builder).Build();
+  MemoryAllocation alloc(&memory_layout);
+
+  {
+    ASSERT_OK_AND_ASSIGN(BoundInputLoader<OuterTestStruct> bound_input_loader,
+                         input_loader->Bind({
+                             {"get(a).a", TypedSlot::FromSlot(a_slot)},
+                             {"get(a).b", TypedSlot::FromSlot(b_slot)},
+                             {"get(b)", TypedSlot::FromSlot(top_b_slot)},
+                         }));
+
+    TestStruct ts{5, 3.5};
+    ASSERT_OK(bound_input_loader({&ts, -1}, alloc.frame()));
+    EXPECT_EQ(alloc.frame().Get(a_slot), 5);
+    EXPECT_EQ(alloc.frame().Get(b_slot), 3.5);
+    EXPECT_EQ(alloc.frame().Get(top_b_slot), -1);
+  }
+
+  {  // partial, both loaders needed
+    ASSERT_OK_AND_ASSIGN(BoundInputLoader<OuterTestStruct> bound_input_loader,
+                         input_loader->Bind({
+                             {"get(a).a", TypedSlot::FromSlot(a_slot)},
+                             {"get(b)", TypedSlot::FromSlot(top_b_slot)},
+                         }));
+
+    alloc.frame().Set(b_slot, -5.7);
+    TestStruct ts{15, 19.0};
+    ASSERT_OK(bound_input_loader({&ts, 57}, alloc.frame()));
+    EXPECT_EQ(alloc.frame().Get(a_slot), 15);
+    EXPECT_EQ(alloc.frame().Get(b_slot), -5.7);  // not updated
+    EXPECT_EQ(alloc.frame().Get(top_b_slot), 57);
+  }
+
+  {  // partial, only one loader
+    ASSERT_OK_AND_ASSIGN(BoundInputLoader<OuterTestStruct> bound_input_loader,
+                         input_loader->Bind({
+                             {"get(a).a", TypedSlot::FromSlot(a_slot)},
+                         }));
+
+    alloc.frame().Set(b_slot, -5.7);
+    alloc.frame().Set(top_b_slot, -57);
+    TestStruct ts{75, 19.0};
+    ASSERT_OK(bound_input_loader({&ts, 51}, alloc.frame()));
+    EXPECT_EQ(alloc.frame().Get(a_slot), 75);
+    EXPECT_EQ(alloc.frame().Get(b_slot), -5.7);     // not updated
+    EXPECT_EQ(alloc.frame().Get(top_b_slot), -57);  // not updated
   }
 }
 
