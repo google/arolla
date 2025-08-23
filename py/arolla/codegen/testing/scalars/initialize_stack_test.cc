ea2a9302c443a96a79c1477efbbc2fdbf133596f
Alexander Pronchenkov <axv@google.com>
2025-08-23 01:36:20 -0700

    Fix initialize_stack_test
    PiperOrigin-RevId: 798501493
    Change-Id: Ic247daf6fb3f02507f34d4cc04d5e1225199458a

--- py/arolla/codegen/testing/scalars/initialize_stack_test.cc
+++ py/arolla/codegen/testing/scalars/initialize_stack_test.cc
@@ -17,13 +17,14 @@
 
 #include <cstdint>
 #include <memory>
-#include <optional>
 #include <string>
 #include <utility>
 
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 #include "absl/container/flat_hash_map.h"
+#include "absl/status/status_matchers.h"
+#include "absl/status/statusor.h"
 #include "absl/strings/str_cat.h"
 #include "arolla/memory/frame.h"
 #include "arolla/memory/memory_allocation.h"
@@ -33,39 +34,50 @@
 #include "arolla/qtype/typed_slot.h"
 #include "py/arolla/codegen/testing/scalars/many_inputs_and_side_outputs.h"
 
+#include <pthread.h>
+
 namespace arolla {
 namespace {
 
+using absl_testing::IsOk;
+
 TEST(StackTest, StackLimitedInit) {
-  auto fn = []() {
+  static constexpr auto fn = [] {
     FrameLayout::Builder layout_builder;
     absl::flat_hash_map<std::string, TypedSlot> inputs;
     for (int64_t i = 0; i != 1000; ++i) {
       inputs.emplace(absl::StrCat("input_", i),
                      TypedSlot::FromSlot(layout_builder.AddSlot<float>()));
     }
-    ASSERT_OK_AND_ASSIGN(
-        std::unique_ptr<BoundExpr> executable,
+    absl::StatusOr<std::unique_ptr<BoundExpr>> executable =
         ::test_namespace::GetManyInputsAndSideOutputs().Bind(
             &layout_builder, inputs,
-            TypedSlot::FromSlot(layout_builder.AddSlot<float>())));
-
+            TypedSlot::FromSlot(layout_builder.AddSlot<float>()));
+    ASSERT_THAT(executable.status(), IsOk());
     FrameLayout memory_layout = std::move(layout_builder).Build();
     MemoryAllocation alloc(&memory_layout);
-    ASSERT_OK(executable->InitializeLiterals(alloc.frame()));
+    ASSERT_THAT((**executable).InitializeLiterals(alloc.frame()), IsOk());
   };
 
-  thread::Options options;
-  options.set_joinable(true);
   // NOTE: stack size has page granularity (4096 bytes on x86), and
   // the thread creation code adds TLS data size to the stack size.
   // As a result, asking for X bytes results in some amount from [X, X+4095].
   // The code here uses much less stack, but we have a loose
   // limit to avoid limiting underlying libraries such as tcmalloc or profiling.
-  options.set_stack_size(16 * 1024);
-  ClosureThread thread(options, "MyThread", fn);
-  thread.Start();
-  thread.Join();
+
+  pthread_attr_t pthread_attr;
+  ASSERT_EQ(pthread_attr_init(&pthread_attr), 0);
+  ASSERT_EQ(pthread_attr_setstacksize(&pthread_attr, 16384), 0);
+  pthread_t pthread;
+  ASSERT_EQ(pthread_create(
+                &pthread, &pthread_attr,
+                [](void*) -> void* {
+                  fn();
+                  return nullptr;
+                },
+                nullptr),
+            0);
+  ASSERT_EQ(pthread_join(pthread, nullptr), 0);
 }
 
 }  // namespace
