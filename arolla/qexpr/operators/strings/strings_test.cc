f7d2cd59f863f748e767675aaaee4cd7970b48ca
Arolla Team <noreply@google.com>
2025-12-22 20:21:02 -0800

    No public description
    PiperOrigin-RevId: 847985814
    Change-Id: Ib2217b4eaa1541bc7e4d53f159ab0e0b409cf299

--- arolla/qexpr/operators/strings/strings_test.cc
+++ arolla/qexpr/operators/strings/strings_test.cc
@@ -17,7 +17,6 @@
 
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
-#include "absl/status/status.h"
 #include "absl/status/status_matchers.h"
 #include "arolla/dense_array/dense_array.h"
 #include "arolla/dense_array/qtype/types.h"
