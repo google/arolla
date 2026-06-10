d7bb0265844484eef94c5c59746d97a81282de04
Alexander Pronchenkov <axv@google.com>
2026-06-10 08:01:43 -0700

    Add a compiler benchmark
    PiperOrigin-RevId: 929856979
    Change-Id: Ic61d0d221bd3e00a3e61ec9d26cbb8146967b0e0

--- arolla/expr/eval/compile_benchmark.cc
+++ arolla/expr/eval/compile_benchmark.cc
@@ -12,10 +12,15 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
+#include <cstddef>
+#include <cstdint>
+#include <utility>
+
 #include "benchmark/benchmark.h"
 #include "absl/strings/string_view.h"
 #include "arolla/expr/eval/model_executor.h"
 #include "arolla/expr/expr.h"
+#include "arolla/expr/optimization/default/default_optimizer.h"
 #include "arolla/io/wildcard_input_loader.h"
 #include "arolla/qexpr/eval_context.h"
 #include "arolla/qtype/base_types.h"
@@ -28,6 +33,7 @@ using ::arolla::InitArolla;
 using ::arolla::WildcardInputLoader;
 using ::arolla::expr::CallOp;
 using ::arolla::expr::CompileModelExecutor;
+using ::arolla::expr::DefaultOptimizer;
 using ::arolla::expr::Leaf;
 using ::arolla::expr::Literal;
 using ::arolla::expr::ModelExecutorOptions;
@@ -95,5 +101,32 @@ void BM_Add_F64_F32_NTimes_WithStacktrace(benchmark::State& state) {
 
 BENCHMARK(BM_Add_F64_F32_NTimes_WithStacktrace);
 BENCHMARK(BM_Add_F64_F32_NTimes_WithoutStacktrace);
+
+template <size_t N>
+void BM_Fib(benchmark::State& state) {
+  InitArolla();
+  auto f0_expr = Literal<int64_t>(0);
+  auto f1_expr = Leaf("x");
+  for (int i = 2; i < N; ++i) {
+    auto f2_expr = *CallOp("math.add", {f0_expr, f1_expr});
+    f0_expr = std::move(f1_expr);
+    f1_expr = std::move(f2_expr);
+  }
+  auto expr = std::move(f1_expr);
+  auto input_loader = *WildcardInputLoader<int64_t>::Build(
+      [](int64_t x, absl::string_view) { return x; });
+  ModelExecutorOptions options;
+  options.eval_options.enable_expr_stack_trace = true;
+  options.eval_options.optimizer = *DefaultOptimizer();
+  for (auto _ : state) {
+    auto model_executor =
+        *CompileModelExecutor<int64_t>(expr, *input_loader, options);
+    auto y = *model_executor.Execute(1);
+    benchmark::DoNotOptimize(y);
+  }
+}
+
+BENCHMARK_TEMPLATE(BM_Fib, 10);
+BENCHMARK_TEMPLATE(BM_Fib, 100);
 
 }  // namespace
