fd14af249a5f4344c7f8c2c292998503795c9735
Arolla Team <noreply@google.com>
2026-08-06 15:18:05 -0700

    fix: Update Bazel release key URL
    PiperOrigin-RevId: 960531526
    Change-Id: Iad13583caa8c077710773dea51b2d686d6abd9ba

--- build_pypi_package/base.sh
+++ build_pypi_package/base.sh
@@ -27,7 +27,7 @@ apt install -y \
     curl \
     gnupg \
     ;
-curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor >bazel-archive-keyring.gpg
+curl -fsSL https://releases.bazel.build/bazel-release.pub.gpg | gpg --dearmor >bazel-archive-keyring.gpg
 mv bazel-archive-keyring.gpg /usr/share/keyrings
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/bazel-archive-keyring.gpg] https://storage.googleapis.com/bazel-apt stable jdk1.8" | tee /etc/apt/sources.list.d/bazel.list
 
